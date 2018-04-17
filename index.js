7812519358b4aaffd7d69e2ba0b2de635756ad5c
Elvis Pham <epha1206@gmail.com>
2018-04-17 19:13:46 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+var word = require("word");
+var inquirer = require("inquirer");
+
+var words = [
+    "bird",
+    "dog",
+    "fish",
+    "lizard"
+];
+
+var user new word(words);
+var dashed = user.randomWord();
+
+function askQuestion() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "character",
+            meassage: "Guess a letter!"
+        }
+    ])
+    .then(function (answers) {
+        var character = answers.character;
+        const userAnswer = answers.character;
+
+        if (user.word.includes(userAnswer)) {
+            var index = user.word.indexOf(userAnswer);
+            dashed[index] = userAnswer;
+            console.log("Here is the word for you to guess " + dashed);
+            var guessword = dashed.join("");
+
+            if (guessword === user.word) {
+                console.log("You Win!!!");
+            }
+            else {
+                askQuestion();
+            }
+        } else {
+            console.log("Sorry, that is incorrect.");
+            askQuestion();
+        }
+    });
+}
+
+askQuestion();
